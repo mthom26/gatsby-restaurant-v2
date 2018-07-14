3862c0035cfd2c3f90ca474a313fd12f7487fce2
mthom26 <mthom2626@gmail.com>
2018-07-14 14:44:29 +0100

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+
+const Footer = () => {
+  return (
+    <Container className="text-light text-center bg-dark" fluid>
+      <Row>
+        <Col md={4}>
+          <p>Contact</p>
+        </Col>
+        <Col md={4}>
+          <p>LOGO</p>
+        </Col>
+        <Col md={4}>
+          <p>Social Icons</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <NavBar />
       {children()}
+      <Footer />
     </div>
   );
 };
