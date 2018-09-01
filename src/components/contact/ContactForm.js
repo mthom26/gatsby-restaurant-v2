a03dff314ce6e64da1c52d45680e305e29ff6ee3
mthom26 <mthom2626@gmail.com>
2018-09-01 13:13:08 +0100

    Refactor form to use AWS Lambda function

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -5,43 +5,101 @@ import {
   FormGroup,
   Label,
   Input,
-  Button
+  Button,
+  UncontrolledAlert
 } from 'reactstrap';
 
 import styles from './ContactForm.module.css';
 
-const ContactForm = ({ data }) => {
-  return (
-    <div className={styles.contactForm}>
-      <div className={`${styles.contactContent}`}>
-        <div className={`d-flex flex-column justify-content-center align-items-center ${styles.formContainer} text-light`}>
-          <h2 className="text-center">Get In Touch</h2>
-          <hr className="bg-primary w-75"/>
-          <Form action={`https://formspree.io/${process.env.GATSBY_FORMSPREE_EMAIL}`} method="POST" className="w-75">
-            <FormGroup>
-              <Label for="name">Name</Label>
-              <Input type="text" name="name" id="name" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input type="email" name="email" id="email" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="message">Message</Label>
-              <Input type="textarea" name="message" id="message" />
-            </FormGroup>
-            <Input type="text" name="_gotcha" style={{ display: 'none' }}/>
-            <Button className="mt-5" color="primary" size="lg" block>Submit</Button>
-          </Form>
+const INITIAL_STATE = {
+  formData: {
+    name: '',
+    email: '',
+    message: ''
+  },
+  formSuccess: false,
+  formFailure: false
+};
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = INITIAL_STATE;
+  }
+
+  onUpdateState = (event) => {
+    const { target } = event;
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [target.name]: target.value
+      }
+    });
+  }
+
+  onSubmit = () => {
+    return fetch(process.env.GATSBY_AWS_CONTACTFORM, {
+      method: "POST",
+      body: JSON.stringify(this.state.formData)
+    })
+      .then(response => {
+        console.log(response);
+        this.setState({ ...INITIAL_STATE, formSuccess: true });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ ...INITIAL_STATE, formFailure: true });
+      });
+  }
+
+  render() {
+    const { name, email, message } = this.state.formData;
+    const { formSuccess, formFailure } = this.state;
+    const { data } = this.props;
+
+    return (
+      <div className={styles.contactForm}>
+        <div className={`${styles.contactContent}`}>
+          <div className={`d-flex flex-column justify-content-center align-items-center ${styles.formContainer} text-light`}>
+            <h2 className="text-center">Get In Touch</h2>
+            <hr className="bg-primary w-75"/>
+            <Form className="w-75 mb-5">
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <Input onChange={this.onUpdateState} value={name} type="text" name="name" id="name" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input onChange={this.onUpdateState} value={email} type="email" name="email" id="email" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="message">Message</Label>
+                <Input onChange={this.onUpdateState} value={message} type="textarea" name="message" id="message" />
+              </FormGroup>
+              <Input type="text" name="_gotcha" style={{ display: 'none' }}/>
+              <Button onClick={this.onSubmit} className="mt-5" color="primary" size="lg" block>Submit</Button>
+            </Form>
+            {formSuccess && (
+              <UncontrolledAlert color="success">
+                Form successfully sent!
+              </UncontrolledAlert>
+            )}
+            {formFailure && (
+              <UncontrolledAlert color="danger">
+                Sorry there was a problem sending your message.
+              </UncontrolledAlert>
+            )}
+          </div>
         </div>
+        <Img
+          outerWrapperClassName="h-100"
+          style={{height: '100%'}}
+          sizes={data.childImageSharp.sizes}
+        />
       </div>
-      <Img
-        outerWrapperClassName="h-100"
-        style={{height: '100%'}}
-        sizes={data.childImageSharp.sizes}
-      />
-    </div>
-  );
+    );
+  }
 };
 
 export default ContactForm;
